d976edf30e866cd7308aee30b61a79561fd5b7d2
Erick Vilcica <erick.vilcica@protonmail.com>
2024-10-30 12:34:43 +0000

    chore(db): set up basic tables

---
+++ database/migrations/2024_10_30_121624_create_reviews_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->float('rating');
+            $table->string('subject')->nullable();
+            $table->string('body')->nullable();
+            $table->timestamps();
+        });
+
+        DB::statement("ALTER TABLE reviews ADD CONSTRAINT chk_rating_range CHECK (rating >= 0.0 AND rating <= 5.0)");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
