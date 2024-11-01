9604cc14888c7db3f696b4507ac3a73e7e91e344
Erick Vilcica <erick.vilcica@protonmail.com>
2024-11-01 16:06:36 +0000

    chore(db): create rest of database

---
+++ database/migrations/2024_11_01_155656_create_cart_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained('users', 'id', 'idx_cart_u_id');
+            $table->foreignId('product_id')->constrained('products', 'id', 'idx_cart_p_id');
+            $table->timestamps();
+
+            $table->primary(['user_id', 'product_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart');
+    }
+};
---
+++ database/migrations/2024_11_01_162049_create_orders_products_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders_products', function (Blueprint $table) {
+            $table->foreignId('order_id')->constrained('orders', 'id', 'idx_orders_products_o_id');
+            $table->foreignId('product_id')->constrained('products', 'id', 'idx_orders_products_p_id');
+            $table->timestamps();
+
+            $table->primary(['order_id', 'product_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders_products');
+    }
+};
