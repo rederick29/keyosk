15557fe7be72cd07ec3f580d74e5d1f22c22ee9a
BenSnaith <bsnaith.bs@icloud.com>
2024-11-04 22:21:44 +0000

    add keyosk logo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,7 +22,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     {{-- Title can be passed as <x-slot:title>, however by default it will use "Keytamine" --}}
-    <title>{{ $title ?? "Keytamine" }}</title>
+    <title>{{ $title ?? "Keyosk" }}</title>
 </head>
 <body class="bg-zinc-950 relative">
     <x-navbar />
