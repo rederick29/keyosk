edcaf735481fe2512747507dca5f2c078c9274b3
intns <84647527+intns@users.noreply.github.com>
2024-11-05 18:55:12 +0000

    Allow inline-scripts using nonce

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -18,10 +18,14 @@ class SecurityHeaders
         // Get the response from the next middleware/request handler
         $response = $next($request);
 
+        $nonce = base64_encode(random_bytes(16));
+
         // Set security headers
-        $response->headers->set('Content-Security-Policy', "frame-ancestors 'none'; default-src 'self'; script-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self'; upgrade-insecure-requests;");
+        $response->headers->set('Content-Security-Policy', "frame-ancestors 'none'; default-src 'self'; script-src 'self' 'nonce-{$nonce}'; object-src 'none'; base-uri 'self'; form-action 'self'; upgrade-insecure-requests;");
         $response->headers->set('X-Frame-Options', 'DENY');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
+
+        $request->session()->put('csp_nonce', $nonce);
 
         // Return the modified response
         return $response;
