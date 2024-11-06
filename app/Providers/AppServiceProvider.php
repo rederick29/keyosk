09bd3dcf4a742c0e5cbf1e7864de4fe0f3e7607b
intns <84647527+intns@users.noreply.github.com>
2024-11-06 14:12:33 +0000

    Attempt to circumvent APP_DEBUG with debugbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Barryvdh\Debugbar\Facade as Debugbar;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Disable Debugbar in production
+        if (app()->environment('production')) {
+            Debugbar::disable();
+        }
     }
 }
