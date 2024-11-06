2c8b0e0b065eda2cba18c8e11c82d5b0f7f1882e
M0STN <160626150+M0STN@users.noreply.github.com>
2024-11-06 21:21:56 +0000

    Update about-us.blade.php

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1,27 +1,20 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+=======
+{{--
+    About us page to be used as a view on website.
 
-        <title>About us</title>
+    Author(s): Mohamed Ahmed
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-        @vite("resources/css/app.css")
-    </head>
+    Needs more content added
+--}}
 </head>
-<body class="bg-zinc-950 text-white">
-    <x-navbar />
-    <div class = "about-us pt-44">
+<x-layout>
+    <div class = "about-us bg-zinc-800 pt-32 text-white">
         
         <h1 class = "text-center text-xl">About Us</h1>
-        <p class = "text-center">Keyosk is a multi-product company that is dedicated to providing a top-notch experience to our customers. We take pride in the quality of our products and always strive towards innovation in the peripherals market. We have a strong focus on sustainability and want to make sure we contribute to saving the environment. </p>
+      
+        <p class = "text-center pt-10">Keyosk is a multi-product company that is dedicated to providing a top-notch experience to our customers. We take pride in the quality of our products and always strive towards innovation in the peripherals market. We have a strong focus on sustainability and want to make sure <strong>we</strong> contribute to saving the environment. </p>
         
         
     </div>
-</body>
-</html>
+</x-layout>
+>>>>>>> Stashed changes
