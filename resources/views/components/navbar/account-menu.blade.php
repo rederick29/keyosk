5db211668239d81e4e11187d1ac5244f0baafb88
BenSnaith <bsnaith.bs@icloud.com>
2024-11-06 01:19:56 +0000

    fix(navbar): dropdown menu auto hide behaviour

--- resources/views/components/navbar/account-menu.blade.php
+++ resources/views/components/navbar/account-menu.blade.php
@@ -7,7 +7,7 @@
     TODO: fix dropdown animation
 --}}
 
-<div class="flex flex-row items-center justify-center p-2 rounded-md transition-colors duration-300 relative" id="account-icon">
+<div class="flex flex-row items-center justify-center p-2 rounded-md ring-violet-700 hover:bg-white/5 transition-colors duration-300 relative" id="account-icon">
     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
     <div class="hidden lg:inline md:inline">
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M6 9l6 6 6-6"/></svg>
@@ -28,21 +28,38 @@
     <script nonce="{{ session("csp_nonce") }}">
         let accountIcon = document.getElementById('account-icon');
         let accountDropdown = document.getElementById('account-dropdown');
-        let accountDropdownToggle = false;
+        let accountToggle = false;
 
-        accountIcon.addEventListener("click", function() {
-            if(!accountDropdownToggle) {
-                accountIcon.classList.add("bg-white/5", "shadow-inner")
-                accountDropdown.classList.remove('dropdown-hide-desktop');
-                accountDropdown.classList.add('dropdown-display-desktop');
-                accountDropdownToggle = true;
-            }
-            else {
-                accountIcon.classList.remove("bg-white/5", "shadow-inner")
-                accountDropdown.classList.remove('dropdown-display-desktop');
-                accountDropdown.classList.add('dropdown-hide-desktop');
-                accountDropdownToggle = false;
+        document.body.addEventListener("click", function(e) {
+            if(accountToggle) {
+                hideMenu();
             }
         });
+        //document.body.addEventListener("click", () => hideMenu());
+
+        accountIcon.addEventListener("click", function(event) {
+            if(!accountToggle) {
+                showMenu();
+            }
+            else {
+                hideMenu();
+            }
+            // stopPropagation must be called on the parent / spawner element.
+            event.stopPropagation();
+        });
+
+        const showMenu = () => {
+            accountIcon.classList.add("bg-white/5", "ring-2")
+            accountDropdown.classList.remove('dropdown-hide-desktop');
+            accountDropdown.classList.add('dropdown-display-desktop');
+            accountToggle = true;
+        }
+
+        const hideMenu = () => {
+            accountIcon.classList.remove("bg-white/5", "ring-2")
+            accountDropdown.classList.remove('dropdown-display-desktop');
+            accountDropdown.classList.add('dropdown-hide-desktop');
+            accountToggle = false;
+        }
     </script>
 @endonce
