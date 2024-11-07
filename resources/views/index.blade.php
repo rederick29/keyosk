d4415274d48ee03e5e89d11e9a9f2a1937332a4d
Ben Snaith <bsnaith@icloud.com>
2024-11-07 21:43:48 +0000

    feat(frontend): added footer and logo components

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,5 +8,7 @@
 <x-layouts.layout>
     {{-- Ability to slot in a title to the layout for modularity --}}
     {{-- <x-slot:title>Example Title</x-slot:title> --}}
-    <x-imagescroll></x-imagescroll>
+    <main class="h-screen">
+        <x-util.imagescroll></x-util.imagescroll>
+    </main>
 </x-layouts.layout>
