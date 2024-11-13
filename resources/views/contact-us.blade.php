b600d1280a42654d8b865dd2d139bb9bd8136a67
Kai <kchima671@gmail.com>
2024-11-13 11:36:11 +0000

    edited(contact-us) looks nicer now :D

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -10,7 +10,7 @@
 
 <x-layouts.layout>
 <x-slot:title>Keyosk | Contact Us</x-slot:title>
-<div class="bg-gradient-to-tr from-violet-500 to-pink-500 px-6 py-20 pt-35 lg:pt-40 lg:px-80">
+<div class="bg-zinc-950 px-6 py-20 pt-35 lg:pt-40 lg:px-80">
   <div class="mx-auto max-w-2xl text-center">
     <h2 class="text-balance text-4xl tracking-tight text-white lg:text-5xl">Contact Us</h2>
     <p class="mt-2 text-lg/8 text-violet-700">Enter details below</p>
@@ -19,38 +19,38 @@
     @csrf
     <div class="grid grid-cols-1 gap-x-8 gap-y-5 lg:grid-cols-2">
       <div>
-        <label for="first-name" class="block text-lg/6 text-white text-sm">First name</label>
+        <label for="first-name" class="block font-semibold text-lg/6 text-white text-sm">First name</label>
         <div class="mt-2.5">
-          <input type="text" name="first-name" id="first-name" class="block w-full rounded-lg border border-neutral-100 px-3.5 py-2 text-white bg-zinc-700" required>
+          <input type="text" name="first-name" id="first-name" class="block font-semibold w-full rounded-lg  px-3.5 py-2 text-white bg-zinc-700" required>
         </div>
       </div>
       <div>
-        <label for="last-name" class="block text-lg/6 text-white text-sm">Last name</label>
+        <label for="last-name" class="block font-semibold text-lg/6 text-white text-sm">Last name</label>
         <div class="mt-2.5">
-          <input type="text" name="last-name" id="last-name" class="block w-full rounded-lg border border-neutral-100 px-3.5 py-2 text-white bg-zinc-700" required>
+          <input type="text" name="last-name" id="last-name" class="block font-semibold w-full rounded-lg  px-3.5 py-2 text-white bg-zinc-700" required>
         </div>
       </div>
       <div class="lg:col-span-2">
-        <label for="email" class="block text-lg/6 text-white text-sm">Email</label>
+        <label for="email" class="block font-semibold text-lg/6 text-white text-sm">Email</label>
         <div class="mt-2.5">
-          <input type="email" name="email" id="email" class="block w-full rounded-lg border border-neutral-100 px-3.5 py-2 text-white bg-zinc-700" required>
+          <input type="email" name="email" id="email" class="block font-semi bold w-full rounded-lg  px-3.5 py-2 text-white bg-zinc-700" required>
         </div>
       </div>
       <div class="lg:col-span-2">
-        <label for="subject" class="block text-lg/6 text-white text-sm">Subject</label>
+        <label for="subject" class="block font-semibold  text-lg/6 text-white text-sm">Subject</label>
         <div class="mt-2.5">
-          <input type="text" name="subject" id="subject" class="block w-full rounded-lg border border-neutral-100 px-3.5 py-2 text-white bg-zinc-700">
+          <input type="text" name="subject" id="subject" class="block font-semibold w-full rounded-lg  px-3.5 py-2 text-white bg-zinc-700">
         </div>
       </div>
       <div class="lg:col-span-2">
-        <label for="message" class="block text-lg/6 text-white text-sm">Message</label>
+        <label for="message" class="block font-semibold text-lg/6 text-white text-sm">Message</label>
         <div class="mt-2.5">
-          <textarea name="message" id="message" rows="4" class="block w-full rounded-lg border border-neutral-100 px-3.5 py-2 text-white placeholder:text-white-400 bg-zinc-700" minlength="10" maxlength="250" required></textarea>
+          <textarea name="message" id="message" rows="4" class="block font-semibold w-full rounded-lg  px-3.5 py-2 text-white placeholder:text-white-400 bg-zinc-700" minlength="10" maxlength="250" required></textarea>
         </div>
       </div>
     </div>
     <div class="mt-10">
-      <button type="submit" name="submit" class="block w-full rounded-lg bg-violet-700 px-3.5 py-2.5 text-center text-base text-white hover:bg-violet-500" onSubmit="msgSent()">Submit</button>
+      <button type="submit" name="submit" class="block font-semi bold w-full rounded-lg bg-violet-700 px-3.5 py-2.5 text-center text-base text-white hover:bg-violet-500" onSubmit="msgSent()">Submit</button>
     </div>
   </form>
   <div id="sentPopup" class="hidden p-4 mt-1 mb-4 text-md text-green-400 rounded-md bg-gray-700">
