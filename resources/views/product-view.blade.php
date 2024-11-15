7b0ff789c0e8b9a84f5d9c209754b5c7980bc5d9
Kai <kchima671@gmail.com>
2024-11-15 15:51:53 +0000

    product page progress (PPP)

--- resources/views/product-view.blade.php
+++ resources/views/product-view.blade.php
@@ -1,40 +1,62 @@
 {{--
-    Product view page to be used as a view on website.
+Product view page to be used as a view on website.
 
-    Author(s): Kai Chima : Main Developer
+Author(s): Kai Chima : Main Developer
 
 --}}
 
 <x-layouts.layout>
-<x-slot:title>Keyosk | Product View</x-slot:title>  
-<div class="bg-zinc-900 py-28 px-10 max-w-4xl lg:max-w-7xl mx-auto">
-    <div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
-        <div class="pt-10 sm:flex">
-            <img  class="w-70 rounded-sm object-cover" src="https://th.bing.com/th/id/R.2dd71b9661e8f903644f7e681f5fa6d6?rik=QGpKKR64kRogrQ&pid=ImgRaw&r=0" alt="Product image">
-        </div>
-        <div class="text-white space-y-3">
-            <h2 class="pt-10  text-5xl">Black RGB Keyboard</h2>
-            <p class="text-4xl">£60</p>
-            <div class="flex space-x-1">
-                <svg class="w-14 h-14 fill-yellow-600" viewBox="0 -19 550 550" xmlns="https://www.w3.org.2000/svg">
-                    <path d="M181 286L64 188 218 176 275 30 333 176 486 188 369 286 407 436 275 354 144 440 181 286Z"/>
-                </svg>
-                <svg class="w-14 h-14 fill-yellow-600" viewBox="0 -19 550 550" xmlns="https://www.w3.org.2000/svg">
-                    <path d="M181 286L64 188 218 176 275 30 333 176 486 188 369 286 407 436 275 354 144 440 181 286Z"/>
-                </svg>
-                <svg class="w-14 h-14 fill-yellow-600" viewBox="0 -19 550 550" xmlns="https://www.w3.org.2000/svg">
-                    <path d="M181 286L64 188 218 176 275 30 333 176 486 188 369 286 407 436 275 354 144 440 181 286Z"/>
-                </svg>
-                <svg class="w-14 h-14 fill-yellow-600" viewBox="0 -19 550 550" xmlns="https://www.w3.org.2000/svg">
-                    <path d="M181 286L64 188 218 176 275 30 333 176 486 188 369 286 407 436 275 354 144 440 181 286Z"/>
-                </svg>
-                <svg class="w-14 h-14 fill-yellow-600" viewBox="0 -19 550 550" xmlns="https://www.w3.org.2000/svg">
-                    <path d="M181 286L64 188 218 176 275 30 333 176 486 188 369 286 407 436 275 354 144 440 181 286Z"/>
-                </svg>
+    <x-slot:title>Keyosk | Product View</x-slot:title>
+    <div class="bg-zinc-900/75 py-28 px-10 max-w-4xl lg:max-w-7xl lg:mx-14">
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
+            <div class="pt-10 sm:flex">
+                <img class="flex lg:pl-10 w-70 rounded-sm object-cover"
+                    src="https://th.bing.com/th/id/R.2dd71b9661e8f903644f7e681f5fa6d6?rik=QGpKKR64kRogrQ&pid=ImgRaw&r=0"
+                    alt="Product image">
+            </div>
+            <div class="text-white lg:pl-2 space-y-3">
+                <h2 class="pt-10 font-semibold text-4xl">Black RGB Keyboard</h2>
+                <p class="text-3xl">£60</p>
+                <div class="flex space-x-1">
+                    <svg class="w-8 h-8 fill-yellow-400" viewBox="0 -19 550 550" xmlns="https://www.w3.org.2000/svg">
+                        <path
+                            d="M181 286L64 188 218 176 275 30 333 176 486 188 369 286 407 436 275 354 144 440 181 286Z" />
+                    </svg>
+                    <svg class="w-8 h-8 fill-yellow-400" viewBox="0 -19 550 550" xmlns="https://www.w3.org.2000/svg">
+                        <path
+                            d="M181 286L64 188 218 176 275 30 333 176 486 188 369 286 407 436 275 354 144 440 181 286Z" />
+                    </svg>
+                    <svg class="w-8 h-8 fill-yellow-400" viewBox="0 -19 550 550" xmlns="https://www.w3.org.2000/svg">
+                        <path
+                            d="M181 286L64 188 218 176 275 30 333 176 486 188 369 286 407 436 275 354 144 440 181 286Z" />
+                    </svg>
+                    <svg class="w-8 h-8 fill-yellow-400" viewBox="0 -19 550 550" xmlns="https://www.w3.org.2000/svg">
+                        <path
+                            d="M181 286L64 188 218 176 275 30 333 176 486 188 369 286 407 436 275 354 144 440 181 286Z" />
+                    </svg>
+                    <svg class="w-8 h-8 fill-yellow-400" viewBox="0 -19 550 550" xmlns="https://www.w3.org.2000/svg">
+                        <path
+                            d="M181 286L64 188 218 176 275 30 333 176 486 188 369 286 407 436 275 354 144 440 181 286Z" />
+                    </svg>
+                    <p class="text-white text-xl pl-4 underline">50 Reviews</p>
+                </div>
+                <div class="">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores ipsam esse harum non,
+                        nesciunt odio cupiditate dolores officiis provident vero nihil, facilis modi maiores magni eaque
+                        dolorem libero nostrum consequuntur.</p>
+                </div>
+                <div class="flex flex-wrap pt-3 gap-6 w-25">
+                    <button
+                        class="w-2/5 px-7 py-2 rounded-sm bg-white hover:bg-zinc-200 text-md text-violet-700 text-xl shadow-md ">Add
+                        to cart</button>
+                    <button
+                        class="w-2/5 px-7 py-2 rounded-sm bg-violet-700 hover:bg-violet-500 text-md text-white text-xl shadow-md ">Buy
+                        now</button>
+                </div>
+                
             </div>
         </div>
     </div>
-</div>
 
 
 
