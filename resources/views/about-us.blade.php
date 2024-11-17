a10baca917618d69aa29182fa6ca35e9a1e818fd
M0STN <160626150+M0STN@users.noreply.github.com>
2024-11-17 20:32:27 +0000

    Update about-us page

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -4,17 +4,78 @@
 
     Author(s): Mohamed Ahmed
 
-    TODO: More content to be added as more is done of the website
+    TODO: -Add visuals
+          -Add a new font
 --}}
 
 <x-layouts.layout>
     <x-slot:title>Keyosk | About us</x-slot:title>
-    <div class = "about-us bg-zinc-800 pt-32 text-white">
+    <div class = "about-us bg-zinc-800 pt-32 text-white font-sans">
 
-        <h1 class = "text-center text-xl">About Us</h1>
-
-        <p class = "text-center pt-10">Keyosk is a multi-product company that is dedicated to providing a top-notch experience to our customers. We take pride in the quality of our products and always strive towards innovation in the many markets we pursue, especially peripherals. We have a strong focus on sustainability and want to make sure <strong>we</strong> contribute to saving the environment. </p>
-
-
+        <h1 class = "text-center text-4xl font-semibold">About Us</h1>
+        
+        <div class="bg-zinc-700 p-6 rounded-lg mt-10 mx-auto max-w-4xl">
+            <h1 class = "text-center text-2xl">The Keyosk Mission</h1>
+            <p class = "text-center">Our mission at Keyosk is a dedication to providing an unforgettable experience to our customers. We take pride in the quality of our products and always strive towards innovation in the many markets we pursue, especially peripherals. We have a strong focus on sustainability and want to make sure <strong>we</strong> contribute to saving the environment. </p>
+        </div>
+        <div class="bg-zinc-700 p-6 rounded-lg mt-10 mx-auto max-w-4xl">
+        <h1 class = "text-center text-2xl">The Keyosk Vision</h1>
+        <p class = "text-center">The vision at Keyosk has always been to redefine how people connect with technology, as we look towards designing our products through progressive, ergonomic and sustainable methods. We visualise a future where we become leading innovators in the peripherals industry, as Keyosk products encourage a higher quality of life, productivity and satisfication for our customers. </p>
+        </div>
+        <div class="bg-zinc-700 p-6 rounded-lg mt-10 mx-auto">
+            <h1 class = "text-center text-2xl">Meet The Team</h1>
+            <div class = "grid grid-cols-2 md:grid-cols-4 gap-4 size-1/2 text-center mx-auto mt-6">
+                <div>
+                    <p class ="text-sm font-semibold">Arun</p>
+                    <img src="https://i.ibb.co/XfQgwQp/Unknown-person.jpg">
+                    <p class ="text-sm">Main Backend<br>Security Support</p>
+                </div>
+                <div>
+                    <p class ="text-sm font-semibold">Ben</p>
+                    <img src="https://i.ibb.co/XfQgwQp/Unknown-person.jpg">
+                    <p class ="text-sm">Main Frontend<br>Backend Support</p>
+                </div>
+                <div>
+                    <p class ="text-sm font-semibold">Erick</p>
+                    <img src="https://i.ibb.co/XfQgwQp/Unknown-person.jpg">
+                    <p class ="text-sm">Main Backend</p>
+                </div>
+                <div>
+                    <p class ="text-sm font-semibold">Josh</p>
+                    <img src="https://i.ibb.co/XfQgwQp/Unknown-person.jpg">
+                    <p class ="text-sm">Main Reports</p>
+                </div>
+                <div>
+                    <p class ="text-sm font-semibold">Kai</p>
+                    <img src="https://i.ibb.co/XfQgwQp/Unknown-person.jpg">
+                    <p class ="text-sm">Main Frontend<br>Design & Backend Support</p>
+                </div>
+                <div>
+                    <p class ="text-sm font-semibold">Mohamed</p>
+                    <img src="https://i.ibb.co/XfQgwQp/Unknown-person.jpg">
+                    <p class ="text-sm">Main Design<br>Frontend Support</p>
+                </div>
+                <div>
+                    <p class ="text-sm font-semibold">Mousa</p>
+                    <img src="https://i.ibb.co/XfQgwQp/Unknown-person.jpg">
+                    <p class ="text-sm">Main Management</p>
+                </div>
+                <div>
+                    <p class ="text-sm font-semibold">Nauman</p>
+                    <img src="https://i.ibb.co/XfQgwQp/Unknown-person.jpg">
+                    <p class ="text-sm">Main Reports<br>Management Support</p>
+                </div>
+                <div>
+                    <p class ="text-sm font-semibold">Suktirath</p>
+                    <img src="https://i.ibb.co/XfQgwQp/Unknown-person.jpg">
+                    <p class ="text-sm">Main Frontend<br>Reports Support</p>
+                </div>
+                <div>
+                    <p class ="text-sm font-semibold">Toms</p>
+                    <img src="https://i.ibb.co/XfQgwQp/Unknown-person.jpg">
+                    <p class ="text-sm">Main Management<br>Reports Support</p>
+                </div>
+            </div>
+        </div>
     </div>
 </x-layouts.layout>
