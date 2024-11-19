da984ff7c5552d09c79cedd7881a691f20e47fd8
thomasjohnny059 <128318310+thomasjohnny059@users.noreply.github.com>
2024-11-19 21:54:37 +0000

    Key Cards
    Key card component

---
+++ resources/views/components/util/item-card.blade.php
@@ -0,0 +1,15 @@
+<div class="relative rounded-lg bg-zinc-900 shadow-lg w-80 h-80 max-w-full text-white overflow-hidden transition-transform transform hover:scale-105 mt-1 mb-4 mx-4">
+    <div class="h-3/4 w-full"> <!-- Image takes 3/4 of the height -->
+        <img src="https://picsum.photos/80" alt="Top Image" class="w-full h-full object-cover rounded-t-lg">
+    </div>
+
+    <div class="h-1/4 w-full p-4 flex flex-col justify-center items-center bg-zinc-900 rounded-b-lg"> <!-- Text takes 1/4 of the height -->
+        <div class="text-center">
+            <h3 class="text-lg font-semibold">Card Title</h3> <!-- Smaller text -->
+            <p class="text-xs text-zinc-400 mt-2">Some description text.</p> <!-- Smaller description text -->
+        </div>
+        <div class="mt-4">
+            {{ $slot }}
+        </div>
+    </div>
+</div>
