4edfce7d276be349732dc4dd9327723531ecf55a
thomasjohnny059 <128318310+thomasjohnny059@users.noreply.github.com>
2024-11-20 12:51:27 +0000

    Card Carousel
    implemented a carousel for the cards for the front page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,119 @@
-{{--
-    Index page of website.
+Author: Toms Xavi
 
-    Author(s): Ben Snaith : Main Developer
---}}
 
 <x-layouts.layout>
-    {{-- Ability to slot in a title to the layout for modularity --}}
-    {{-- <x-slot:title>Example Title</x-slot:title> --}}
     <main class="h-screen">
+        
         <x-util.imagescroll></x-util.imagescroll>
 
-        <div class="flex flex-row">
-        <x-util.item-card />
-        <x-util.item-card />
-        <x-util.item-card />
-        <x-util.item-card />
-        </div>
 
+        <div class="p-4">
+            <div class="relative">
+
+                <button
+                    id="scroll-left"
+                    class="absolute left-4 top-1/2 transform -translate-y-1/2 bg-zinc-900 bg-opacity-70 text-white p-4 rounded-full shadow-lg hover:bg-opacity-90 transition duration-300 z-20"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
+                    </svg>
+                </button>
+
+                <div
+                    id="scroll-container"
+                    class="flex overflow-x-auto space-x-4 p-4 bg-zinc-900 rounded-lg scrollbar-hide"
+                >
+
+                    <div class="flex-shrink-0 w-80">
+                        <x-util.item-card />
+                    </div>
+                    <div class="flex-shrink-0 w-80">
+                        <x-util.item-card />
+                    </div>
+                    <div class="flex-shrink-0 w-80">
+                        <x-util.item-card />
+                    </div>
+                    <div class="flex-shrink-0 w-80">
+                        <x-util.item-card />
+                    </div>
+                    <div class="flex-shrink-0 w-80">
+                        <x-util.item-card />
+                    </div>
+                    <div class="flex-shrink-0 w-80">
+                        <x-util.item-card />
+                    </div>
+                </div>
+
+
+                <button
+                    id="scroll-right"
+                    class="absolute right-4 top-1/2 transform -translate-y-1/2 bg-zinc-900 bg-opacity-70 text-white p-4 rounded-full shadow-lg hover:bg-opacity-90 transition duration-300 z-20"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+                    </svg>
+                </button>
+            </div>
+        </div>
     </main>
+
+
+    <script>
+        const container = document.getElementById('scroll-container');
+        const scrollLeftBtn = document.getElementById('scroll-left');
+        const scrollRightBtn = document.getElementById('scroll-right');
+        
+        let scrollInterval;
+        const scrollAmount = 3;  
+        const pauseTime = 2000;  
+
+
+        function performScroll() {
+            container.scrollBy({ left: 80 * scrollAmount, behavior: 'smooth' });  
+        }
+
+
+        function startLoopingScroll() {
+            scrollInterval = setInterval(() => {
+                performScroll(); 
+                clearInterval(scrollInterval); 
+                setTimeout(() => {
+                    startLoopingScroll(); 
+                }, pauseTime); 
+            }, 1000); 
+        }
+
+
+        startLoopingScroll();
+
+
+        scrollLeftBtn.addEventListener('click', () => {
+            clearInterval(scrollInterval);
+            container.scrollBy({ left: -300, behavior: 'smooth' });
+
+
+            setTimeout(startLoopingScroll, 1000);
+        });
+
+  
+        scrollRightBtn.addEventListener('click', () => {
+            clearInterval(scrollInterval); 
+            container.scrollBy({ left: 300, behavior: 'smooth' });
+
+
+            setTimeout(startLoopingScroll, 1000);
+        });
+    </script>
 </x-layouts.layout>
+
+<style>
+
+    #scroll-container::-webkit-scrollbar {
+        display: none;
+    }
+
+    #scroll-container {
+        -ms-overflow-style: none;  
+        scrollbar-width: none;     
+    }
+</style>
