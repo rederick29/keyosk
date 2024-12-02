2f91cc6b6d812322ca35cf0d2e3844ba47cbe25f
thomasjohnny059 <128318310+thomasjohnny059@users.noreply.github.com>
2024-12-02 11:48:28 +0000

    add csp nonce

--- resources/views/components/carousel/carousel.blade.php
+++ resources/views/components/carousel/carousel.blade.php
@@ -46,7 +46,7 @@
         </div>
     </main>
 
-    <script>
+    <script nonce="{{ csp_nonce() }}">
         const container = document.getElementById('scroll-container');
         const scrollLeftBtn = document.getElementById('scroll-left');
         const scrollRightBtn = document.getElementById('scroll-right');
