30a6f846fa5c819dc953993a2be42ccf60dcf314
thomasjohnny059 <128318310+thomasjohnny059@users.noreply.github.com>
2024-12-02 11:59:38 +0000

    added comments back

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,15 @@
+{{--
+    Index page of website.
+
+    Author(s): Ben Snaith : Main Developer, Toms Xavi : Developer
+--}}
+
 <x-layouts.layout>
+    {{-- Ability to slot in a title to the layout for modularity --}}
+    {{-- <x-slot:title>Example Title</x-slot:title> --}}
     <main class="h-screen">
         <x-util.imagescroll></x-util.imagescroll>
         <x-carousel.carousel></x-carousel.carousel>
     </main>
 </x-layouts.layout>
+
