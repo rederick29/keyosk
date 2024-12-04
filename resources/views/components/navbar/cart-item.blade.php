c91d8037ec92e6e9ab0189d1178e26c820bddebe
Ben Snaith <bsnaith.bs@icloud.com>
2024-12-04 18:06:37 +0000

    fix: cart item layout

--- resources/views/components/navbar/cart-item.blade.php
+++ resources/views/components/navbar/cart-item.blade.php
@@ -6,18 +6,21 @@
     TODO:
 --}}
 
-<div class="flex flex-row gap-5 items-center w-full p-2 text-center rounded-lg hover:bg-white/5 transition-colors duration-500">
+<div class="w-full flex flex-row gap-5 items-center p-2 text-center rounded-lg hover:bg-white/5 transition-colors duration-500">
     <div class="flex flex-row items-center w-fit space-x-5 font-semibold text-nowrap overflow-hidden">
         <img src="{{ $productImage }}" alt="{{ $productImage }}" width="100" height="100" class="rounded-sm" />
     </div>
-    <div class="flex flex-col w-full items-center space-y-1 start-0">
-        <h1 class="w-full flex font-medium ">{{ $productTitle }}</h1>
-        <div class="w-full flex flex-row justify-between align-center items-center">
-            <p class="font-bold">£{{ $productPrice }}</p>
-            <p class="text-xs">Qty: {{ $productQuantity }}</p>
+    <div class="w-full">
+        <div class="flex flex-col w-full items-center space-y-1 start-0">
+            <h1 class="w-full flex font-bold">{{ $productTitle }}</h1>
+            <p class="w-full flex font-semibold">£{{ $productPrice }}</p>
         </div>
-        <button class="w-fit flex items-start justify-start mt-1 text-xs text-red-600 hover:underline hover:text-red-700">
-            remove
-        </button>
+        <div class="flex flex-row w-full justify-end items-center">
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+
+            <span>{{ $productQuantity }}</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+        </div>
     </div>
 </div>
