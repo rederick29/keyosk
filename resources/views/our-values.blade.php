3147c690e392bf3e00c71b7ea6792807514a1dec
intns <84647527+intns@users.noreply.github.com>
2024-12-06 20:40:47 +0000

    fix: Fix various bugs (phone compatibility)

--- resources/views/our-values.blade.php
+++ resources/views/our-values.blade.php
@@ -6,25 +6,28 @@
 
 <x-layouts.layout>
     <x-slot:title>Keyosk | Our Values</x-slot:title>
-    <div class = "about-us bg-zinc-950 pt-32 text-white font-sans">
+    <div class="bg-zinc-950 text-white font-sans pt-32">
         <h1 class="text-center text-4xl font-extrabold text-purple tracking-wide drop-shadow-lg">
             Our Values
         </h1>
 
-        <div
-            class="bg-gradient-to-br from-zinc-800 via-zinc-900 to-black p-8 rounded-xl shadow-lg mt-10 mx-auto max-w-2xl mb-16">
-            <h1 class="text-center text-2xl font-bold">The Keyosk Price Principle</h1>
-            <br>
-            <p class="text-center mx-auto w-3/4 py-2 leading-relaxed">
-                At Keyosk, we\'re driven by a mission to redefine excellence - delivering cutting-edge, high-quality
-                peripherals that elevate your experience.
-                <br><br>
-                Our commitment is simple: bring unbeatable value to <strong>everyone</strong>. We believe premium
-                products <i>shouldn\'t</i> come with a premium price tag.
-                <br><br>
-                Beyond competitive pricing, we stand by durability and dependability, ensuring our products are built to
-                go the distance and further - because <strong>true value is measured over time.</strong>
-            </p>
+        <div class="px-8">
+            <div
+                class="bg-gradient-to-br from-zinc-800 via-zinc-900 to-black pt-8 pb-8 rounded-xl shadow-lg mt-10 mx-auto max-w-2xl mb-16">
+                <h1 class="text-center text-2xl font-bold">The Keyosk Price Principle</h1>
+                <br>
+                <p class="text-center mx-auto w-3/4 py-2 leading-relaxed">
+                    At Keyosk, we're driven by a mission to redefine excellence - delivering cutting-edge, high-quality
+                    peripherals that elevate your experience.
+                    <br><br>
+                    Our commitment is simple: bring unbeatable value to <strong>everyone</strong>. We believe premium
+                    products <i>shouldn't</i> come with a premium price tag.
+                    <br><br>
+                    Beyond competitive pricing, we stand by durability and dependability, ensuring our products are
+                    built to
+                    go the distance and further - because <strong>true value is measured over time.</strong>
+                </p>
+            </div>
         </div>
 
 </x-layouts.layout>
