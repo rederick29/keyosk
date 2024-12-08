ab966baccb643e8774d7ed90a40739dbf2e96760
Kai <kchima671@gmail.com>
2024-12-08 22:43:11 +0000

    tweaking

--- resources/views/product-view.blade.php
+++ resources/views/product-view.blade.php
@@ -35,6 +35,20 @@ Author(s): Kai Chima : Main Developer, Erick Vilcica: Backend developer
                 @endif
                 <div class="">
                     <p>{{ $product->short_description }}</p>
+                    <div class="flex flex-row w-full justify-between items-center">
+            <div class="flex flew-row ring-violet-700 ring-2 rounded-md">
+                <div class="size-7 flex items-center justify-center bg-zinc-700 rounded-bl-md rounded-tl-md">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                </div>
+                <input id="selectQuantity" class="w-10 h-7 px-[0.33rem] flex items-center justify-center bg-zinc-800 outline-none" value="1">
+                <div class="size-7 flex items-center justify-center bg-zinc-700 rounded-br-md rounded-tr-md">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                </div>
+            </div>
+            <div class="size-7 flex items-center justify-center ring-violet-700 ring-2 rounded-md">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+            </div>
+        </div>
                 </div>
                 <div class="flex flex-wrap pt-3 gap-6 w-25">
                     <form class="lg:w-2/5" method="POST" action="{{ route('cart.store') }}">
@@ -70,3 +84,10 @@ Author(s): Kai Chima : Main Developer, Erick Vilcica: Backend developer
         </div>
     </div>
 </x-layouts.layout>
+
+<script>
+    function getQuantity() {
+        let quantity = document.getElementById('selectQuantity').getAttribute('value');
+        document.getElementById('quanity').value = quantity;
+    }
+</script>
