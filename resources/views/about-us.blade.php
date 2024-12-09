05618ae363c85aa83c4f954a1db261942b0c8a04
M0STN <160626150+M0STN@users.noreply.github.com>
2024-12-09 12:13:07 +0000

    Update about-us.blade.php

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -14,7 +14,7 @@
 
         <h1 class = "text-center text-4xl font-semibold">About Us</h1>
         
-        <div class="bg-zinc-900 p-6 rounded-lg mt-10 mx-auto max-w-2xl">
+        <div class="bg-stone-100 dark:bg-zinc-950 p-6 rounded-lg mt-10 mx-auto max-w-2xl">
             <h1 class = "text-center text-2xl">The Keyosk Mission</h1>
             <br>
             <p class="text-center mx-auto w-3/4 text-wrap">Our mission at Keyosk is a dedication to providing an unforgettable experience to our customers. We take pride in the quality of our products and always strive towards innovation in the many markets we pursue, especially peripherals. We have a strong focus on sustainability and want to make sure <strong>we</strong> contribute to saving the environment. </p>
@@ -25,7 +25,7 @@
         <br>
         <p class = "text-center mx-auto w-3/4 text-wrap">The vision at Keyosk has always been to redefine how people connect with technology, as we look towards designing our products through progressive, ergonomic and sustainable methods. We visualise a future where we become leading innovators in the peripherals industry, as Keyosk products encourage a higher quality of life, productivity and satisfication for our customers. </p>
         </div>
-        <div class="bg-zinc-900 p-6 rounded-lg mt-10 mx-auto">
+        <div class="bg-stone-100 dark:bg-zinc-950 p-6 rounded-lg mt-10 mx-auto">
             <h1 class = "text-center text-2xl">Meet The Team</h1>
             <div class = "grid grid-cols-2 md:grid-cols-4 gap-7 size-3/5 md:size-3/5 text-center mx-auto mt-6">
                 <x-util.team-card name="Arun" role="Backend Developer" support="Security Support" initials="AW"/>
