04997f9e2c663fd7d95d7c32c9593a87efec963b
M0STN <160626150+M0STN@users.noreply.github.com>
2024-12-09 13:39:49 +0000

    Updated team card

--- resources/views/components/util/team-card.blade.php
+++ resources/views/components/util/team-card.blade.php
@@ -1,8 +1,7 @@
-<div class="relative w-full max-w-[190px] min-w-full aspect-w-4 aspect-h-5 bg-transparent p-4">
+<div class="relative  w-full max-w-[190px] max-h-[200px] min-w-full aspect-w-4 aspect-h-5  p-4">
 
-    <svg class="absolute inset-0 w-full h-full" viewBox="0 0 204 258" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-        <path d="M4 28C4 12.536 16.536 0 32 0H172C187.464 0 200 12.536 200 28V222C200 237.464 187.464 250 172 250H32C16.536 250 4 237.464 4 222V28Z" fill="#6D28D9"/>
-        <path d="M4.5 28C4.5 12.8122 16.8122 0.5 32 0.5H172C187.188 0.5 199.5 12.8122 199.5 28V222C199.5 237.188 187.188 249.5 172 249.5H32C16.8122 249.5 4.5 237.188 4.5 222V28Z" stroke="black"/>
+    <svg class="absolute inset-0 w-full h-full flex bg-orange-500 dark:bg-violet-700 rounded-xl border-black border" viewBox="0 0 204 258" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+        <path d="28C4 12.536 16.536 0 32 0H172C187.464 0 200 12.536 200 28V222C200 237.464 187.464 250 172 250H32C16.536 250 4 237.464 4 222V28Z" fill="#6D28D9"/>
     </svg>
 
     <div class="relative flex flex-col text-center pb-4">
@@ -10,7 +9,7 @@
             {{ $name }}
         </p>
 
-        <img src="{{ asset('images/initials/' . $initials . '.svg') }}"  class="pt-4">
+        <img src="{{ asset('images/initials/' . $initials . '.svg') }}"  class="">
 
         <div class="pt-4">
             <p class="text-white text-xs sm:text-sm lg:text-sm">
