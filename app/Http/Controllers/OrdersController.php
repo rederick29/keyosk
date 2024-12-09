8e62bd4ba2664225378eee72c599f209a9bd26ce
intns <84647527+intns@users.noreply.github.com>
2024-12-09 01:04:20 +0000

    refactor: Reimplement OrdersController efficiently

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,20 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Order;
-use App\Models\User;
-use App\Models\Product;
-use App\Models\Image;
-use App\Support\Colllection;
+use Illuminate\Support\Facades\Auth;
 
 class OrdersController extends Controller
 {
-    public function index(){
-        $orders = Order::orderBy('created_at', 'DESC')->get();
-        $products = Product::all();
-        $user = User::all();
-        $images = Image::all();
-        return view('/orders', compact('orders', 'products', 'user'));
+    public function index()
+    {
+        // Get all orders for the authenticated user with their products and images order by latest first
+        $orders = Order::with(['user', 'products.images'])
+            ->where('user_id', Auth::id())
+            ->orderBy('created_at', 'DESC')
+            ->get();
+
+        return view('orders', compact('orders'));
     }
 }
