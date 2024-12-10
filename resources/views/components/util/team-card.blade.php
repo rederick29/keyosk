de9304c95f382b7415d02553f64e46f5d5451752
Kai <kchima671@gmail.com>
2024-12-10 19:52:36 +0000

    tweak(team-card): Adjust height

--- resources/views/components/util/team-card.blade.php
+++ resources/views/components/util/team-card.blade.php
@@ -1,4 +1,4 @@
-<div class="relative  w-full max-w-[190px] max-h-[200px] min-w-full aspect-w-4 aspect-h-5  p-4">
+<div class="relative  w-full max-w-[190px] max-h-[235px] min-w-full aspect-w-4 aspect-h-5  p-4">
 
     <svg class="absolute inset-0 w-full h-full flex bg-orange-500 dark:bg-violet-700 rounded-xl border-black border" viewBox="0 0 204 258" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
         <path d="28C4 12.536 16.536 0 32 0H172C187.464 0 200 12.536 200 28V222C200 237.464 187.464 250 172 250H32C16.536 250 4 237.464 4 222V28Z" fill="#6D28D9"/>
