2662d18899bacc4c6c3b25dc5cdff3b7856b1ae0
M0STN <160626150+M0STN@users.noreply.github.com>
2024-12-10 21:23:02 +0000

    Updated team card

--- resources/views/components/util/team-card.blade.php
+++ resources/views/components/util/team-card.blade.php
@@ -1,8 +1,5 @@
-<div class="relative  w-full max-w-[190px] max-h-[235px] min-w-full aspect-w-4 aspect-h-5  p-4">
+<div class="relative w-[190px] h-[235px] p-4 absolute inset-0 flex bg-orange-500 dark:bg-violet-700 rounded-xl border-black border">
 
-    <svg class="absolute inset-0 w-full h-full flex bg-orange-500 dark:bg-violet-700 rounded-xl border-black border" viewBox="0 0 204 258" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-        <path d="28C4 12.536 16.536 0 32 0H172C187.464 0 200 12.536 200 28V222C200 237.464 187.464 250 172 250H32C16.536 250 4 237.464 4 222V28Z" fill="#6D28D9"/>
-    </svg>
 
     <div class="relative flex flex-col text-center pb-4">
         <p class="font-semibold text-white text-xs sm:text-sm lg:text-base">
@@ -12,10 +9,10 @@
         <img src="{{ asset('images/initials/' . $initials . '.svg') }}"  class="">
 
         <div class="pt-4">
-            <p class="text-white text-xs sm:text-sm lg:text-sm">
+            <p class="text-white text-xs sm:text-sm lg:text-sm 2xl:text-base">
                 {{ $role }}
             </p>
-            <p class="text-white text-[10px] sm:text-xs lg:text-xs ">
+            <p class="text-white text-[10px] sm:text-xs lg:text-xs 2xl:test-sm p-4">
                 {{ $support }}
             </p>
         </div>
