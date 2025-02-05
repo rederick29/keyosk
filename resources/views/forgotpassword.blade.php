a073160a467ed9ba0a13f1669d2073f6842c52bf
MoMousa143 <160495118+MoMousa143@users.noreply.github.com>
2025-02-05 10:23:11 +0000

    Create forgotpassword.blade.php

---
+++ resources/views/forgotpassword.blade.php
@@ -0,0 +1,16 @@
+{{--
+    User profile Forgot Password page.
+
+    Author(s): Mohammed Mousa: Frontend Developer
+--}}
+
+<-layouts.min-layout>
+    <div class="flex flex-col items-center justify-center space-y-10 h-screen w-full bg-gradient-to-tr from-orange-500 to-red-500 dark:from-violet-500 dark:to-pink-500">
+        <div class="h-fit w-11/12 md:w-3/4 lg:w-1/3 p-10 flex flex-col justify-center rounded-2xl bg-stone-100 dark:bg-zinc-900">
+            <div class="w-full flex flex-row items-center justify-center">
+                <x-util.logo type="a" href="/" width=300 />
+            </div>
+
+        </div>
+    </div>
+</x-layouts.min-layout>
