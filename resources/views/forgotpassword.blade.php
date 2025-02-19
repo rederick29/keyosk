a19b0d4baa9a51e11b869dc975e502fb94818323
MoMousa143 <160495118+MoMousa143@users.noreply.github.com>
2025-02-19 10:27:31 +0000

    forgotpassword
    frontend completed

--- resources/views/forgotpassword.blade.php
+++ resources/views/forgotpassword.blade.php
@@ -7,16 +7,14 @@
     <div class="flex flex-col items-center justify-center space-y-10 h-screen w-full bg-gradient-to-tr from-orange-500 to-red-500 dark:from-violet-500 dark:to-pink-500">
         <div class="h-fit w-11/12 md:w-3/4 lg:w-1/3 p-10 flex flex-col justify-center rounded-2xl bg-stone-100 dark:bg-zinc-900">
             <div class="w-full flex flex-row items-center justify-center">
-                <x-util.logo type="a" href="/" width=300 />
+                <x-util.logo type="a" href="/" width=400 />
             </div>
-            <form method="POST" action="/forgotpassword" class="flex flex-col space-y-5 text-center">
+            <form method="POST" action="/forgotpassword" class="flex flex-col text-center">
                 @csrf
-                <div class= "flex flex-col space-y-2 text-xl">
+                <div class= "flex flex-col pt-5 text-xl">
                     <x-auth.form.label for="resetpassword">Reset Password</x-auth.form.label>
                 </div>
-                <div class= "flex flex-col space-y-2"> 
-                    <x-auth.form.label for="reset">To reset password please enter you email address below</x-auth.form.label>
-                </div>
+                <p class= "flex flex-col pt-1 pb-4 text-left pl-1 font-medium text-black/50 dark:text-white/50">To reset password please enter you email address below</p>
                 <div class= "flex flex-col space-y-2 text-lg">
                     <x-auth.form.label for="emailaddress">Email</x-auth.form.label>
                     <x-auth.form.input id="emailaddress" name="emailaddress" required/>
@@ -28,7 +26,6 @@
                     </button>
                 </div>
             </form>
-            <x-util.colormode-switcher></x-util.colormode-switcher>
         </div>
     </div>
 </x-layouts.min-layout>
