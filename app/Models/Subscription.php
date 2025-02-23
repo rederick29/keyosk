5382c6bc6880277a02710bb821b0d1d2279ec8ee
intns <84647527+intns@users.noreply.github.com>
2025-02-23 17:59:37 +0000

    feat: add more functionality to Subscription

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -18,6 +18,9 @@ class Subscription extends Model
         'ends_at' => 'datetime',
     ];
 
+    // TODO: create an event dispatcher for the subscription model to send mails / stat tracking
+    // TODO: payments should be associated with the subscription
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
@@ -26,5 +29,32 @@ class Subscription extends Model
     public function getTierGradient(): string
     {
         return $this->tier->getGradient();
+    }
+
+    public function renew(int $days): self
+    {
+        $this->ends_at = $this->ends_at && $this->ends_at->isFuture()
+            ? $this->ends_at->addDays($days)
+            : now()->addDays($days);
+        $this->save();
+
+        return $this;
+    }
+
+    // If the subscription has started and has no end date / end date is in the future
+    public function isActive(): bool
+    {
+        return $this->started_at->isPast() &&
+               ($this->ends_at === null || $this->ends_at->isFuture());
+    }
+
+    public function isExpired(): bool
+    {
+        return !$this->isActive();
+    }
+
+    public function daysUntilExpiry(): ?int
+    {
+        return $this->ends_at ? now()->diffInDays($this->ends_at) : null;
     }
 }
