fe5463443d82f69161b9f4bffcc6cefd87a8bee6
BenSnaith <bsnaith.bs@icloud.com>
2025-03-11 20:05:43 +0000

    fix(manage-orders): added column headings

--- resources/views/manage-orders.blade.php
+++ resources/views/manage-orders.blade.php
@@ -5,8 +5,13 @@
 
             <!-- header bar -->
             <section class="w-full h-fit p-6 flex items-center justify-between bg-stone-100 dark:bg-zinc-900 text-zinc-800 dark:text-gray-400 rounded-lg">
-                <p>#</p>
-
+                <p class="w-[30px]">#</p>
+                <p class="w-[128px]">Status</p>
+                <p class="w-[200px]">Username</p>
+                <p class="w-[200px]">Order Date</p>
+                <p class="w-[200px]">Completion Date</p>
+                <p class="w-[100px]">Total</p>
+                <div class="w-[36px] bg-transparent"></div>
             </section>
 
             <!-- customers orders -->
