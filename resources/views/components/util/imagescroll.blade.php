92dd65f1d89d077d06f57c4c203bb5ce6da7d921
BenSnaith <bsnaith.bs@icloud.com>
2025-03-12 17:30:08 +0000

    feat: infinite scroll bg

--- resources/views/components/util/imagescroll.blade.php
+++ resources/views/components/util/imagescroll.blade.php
@@ -5,7 +5,8 @@
     Author(s): Ben Snaith : Main Developer
 --}}
 
-<section class="w-full min-h-[70vh] pt-24 flex items-center bg-linear-to-tr from-orange-500 to-red-500 dark:from-violet-500 dark:to-pink-500" id="image-scroll">
+<section class="relative w-full min-h-[70vh] pt-24 flex items-center bg-linear-to-tr from-orange-500 to-red-500 dark:from-violet-500 dark:to-pink-500 overflow-hidden" id="image-scroll">
+    <div class="absolute box w-full top-0"></div>
     <div
         class="h-full mx-32 flex flex-col anim-right"
     >
@@ -19,3 +20,20 @@
         </x-util.button>
     </div>
 </section>
+
+<style>
+    .box {
+        background-image: url("{{ asset('svgs/keyosk_k.svg') }}");
+        height: calc(80px * 20);
+        animation: slide 4s linear infinite;
+    }
+
+    @keyframes slide {
+        from {
+            transform: translateY(0px)
+        }
+        to {
+            transform: translateY(80px);
+        }
+    }
+</style>
