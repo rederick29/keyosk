b5ea65128dad9fce2dfb59ba9e9831bd52faa8d5
BenSnaith <bsnaith.bs@icloud.com>
2025-03-20 23:20:51 +0000

    feat(keyosk+): animated gradient

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -9,7 +9,7 @@
                 <div class="h-64 p-6 flex flex-col justify-between bg-stone-200 dark:bg-zinc-900 rounded-lg">
                     <h1 class="text-xl font-bold">Harness the power of true typing proficiency</h1>
 
-                    <p class="text-violet-500 hover:underline cursor-pointer">Get more from Keyosk with <span class="font-bold">Keyosk+</span></p>
+                    <a href="/keyosk-plus" class="text-violet-500 hover:underline cursor-pointer">Get more from Keyosk with <span class="font-bold">Keyosk+</span></a>
                 </div>
 
                 <!-- Tags and Filters -->
