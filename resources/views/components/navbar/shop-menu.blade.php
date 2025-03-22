593035473cc0c93560c18143aac59695802252ca
BenSnaith <bsnaith.bs@icloud.com>
2025-03-22 15:33:24 +0000

    feat(shop-menu): add search bar to menu

--- resources/views/components/navbar/shop-menu.blade.php
+++ resources/views/components/navbar/shop-menu.blade.php
@@ -8,50 +8,56 @@
     {{-- Drop down menu --}}
     {{-- Desktop and Medium View --}}
     <div class="scale-0 border-2 border-neutral-400 bg-white dark:bg-zinc-900 rounded-sm fixed md:absolute lg:absolute md:rounded-lg lg:rounded-lg shadow-2xl w-[100vw] md:w-72 lg:w-[680px] h-fit top-24 left-0 md:top-13 lg:top-13 md:left-0 lg:left-0" id="shop-dropdown">
-        <div class="w-full flex items-start space-y-1 min-h-[100%] m-4">
-            <section class="w-2/3 pr-5 flex flex-col">
-                <x-util.button type="a" class="items-start justify-normal">
-                    All
-                </x-util.button>
-                <x-util.button type="a" class="items-start justify-normal">
-                    Best Selling
-                </x-util.button>
-                <x-util.button type="a" class="items-start justify-normal">
-                    Sale
-                </x-util.button>
+        <div class="w-full flex flex-col items-start space-y-1 min-h-[100%] m-4 gap-y-3">
+            <section class="w-full pr-8">
+                <x-util.search class="w-full [&_input]:dark:bg-zinc-700" placeholder=""></x-util.search>
             </section>
 
-            <section class="w-2/3 px-5 flex flex-col border-x-4 border-zinc-700">
-                <x-util.button type="a" class="items-start justify-normal">
-                    Keyboards
-                </x-util.button>
-                <x-util.button type="a" class="items-start justify-normal">
-                    Keycaps
-                </x-util.button>
-                <x-util.button type="a" class="items-start justify-normal">
-                    Switches
-                </x-util.button>
-                <x-util.button type="a" class="items-start justify-normal">
-                    Mice
-                </x-util.button>
-                <x-util.button type="a" class="items-start justify-normal">
-                    Mousepads
-                </x-util.button>
-            </section>
+            <section class="w-full flex">
+                <section class="w-2/3 pr-5 flex flex-col">
+                    <x-util.button type="a" class="items-start justify-normal">
+                        All
+                    </x-util.button>
+                    <x-util.button type="a" class="items-start justify-normal">
+                        Best Selling
+                    </x-util.button>
+                    <x-util.button type="a" class="items-start justify-normal">
+                        Sale
+                    </x-util.button>
+                </section>
 
-            <section class="w-2/3 px-5 mr-3 flex flex-col">
-                <x-util.button type="a" class="items-start justify-normal">
-                    Gamers
-                </x-util.button>
-                <x-util.button type="a" class="items-start justify-normal">
-                    Professionals
-                </x-util.button>
-                <x-util.button type="a" class="items-start justify-normal">
-                    Enthusiasts
-                </x-util.button>
-                <x-util.button type="a" class="items-start justify-normal">
-                    Kawaii >.<
-                </x-util.button>
+                <section class="w-2/3 px-5 flex flex-col border-x-4 border-zinc-700">
+                    <x-util.button type="a" class="items-start justify-normal">
+                        Keyboards
+                    </x-util.button>
+                    <x-util.button type="a" class="items-start justify-normal">
+                        Keycaps
+                    </x-util.button>
+                    <x-util.button type="a" class="items-start justify-normal">
+                        Switches
+                    </x-util.button>
+                    <x-util.button type="a" class="items-start justify-normal">
+                        Mice
+                    </x-util.button>
+                    <x-util.button type="a" class="items-start justify-normal">
+                        Mousepads
+                    </x-util.button>
+                </section>
+
+                <section class="w-2/3 px-5 mr-3 flex flex-col">
+                    <x-util.button type="a" class="items-start justify-normal">
+                        Gamers
+                    </x-util.button>
+                    <x-util.button type="a" class="items-start justify-normal">
+                        Professionals
+                    </x-util.button>
+                    <x-util.button type="a" class="items-start justify-normal">
+                        Enthusiasts
+                    </x-util.button>
+                    <x-util.button type="a" class="items-start justify-normal">
+                        Kawaii >.<
+                    </x-util.button>
+                </section>
             </section>
         </div>
     </div>
