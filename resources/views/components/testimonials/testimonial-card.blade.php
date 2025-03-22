1e75877ef54b5f5cc09e35284b75f1f750d10d5f
BenSnaith <bsnaith.bs@icloud.com>
2025-03-22 17:25:55 +0000

    fix: testamonial card mobile

--- resources/views/components/testimonials/testimonial-card.blade.php
+++ resources/views/components/testimonials/testimonial-card.blade.php
@@ -1,5 +1,5 @@
 <div
-    class="w-96 h-fit px-5 py-7 rounded-lg bg-stone-200 dark:bg-zinc-900 hover:ring-4 hover:scale-105 ring-orange-500 dark:ring-violet-700 overflow-hidden transition-shadow transition-transform cursor-default duration-500"
+    class="w-full md:w-96 h-fit px-5 py-7 rounded-lg bg-stone-200 dark:bg-zinc-900 hover:ring-4 hover:scale-105 ring-orange-500 dark:ring-violet-700 overflow-hidden transition-shadow transition-transform cursor-default duration-500"
 >
     <svg class="text-black/60 dark:text-white/60" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
     <h1
